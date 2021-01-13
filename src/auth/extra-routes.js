90f5e473effe30d3fe59a6b626ef89a1c6d20722
fati-ma <fatima.atiyya@yahoo.com>
2021-01-13 15:18:55 +0200

    c-14

--- src/auth/extra-routes.js
+++ src/auth/extra-routes.js
@@ -1,11 +1,30 @@
 const express = require('express');
 
 const router = express.Router();
-// const users = require('../models/users/users-model.js');
 const bearerMiddleware = require('./middleware/bearer-auth.js');
+const permissions = require('./middleware/authorize.js');
 
 router.get('/secret', bearerMiddleware, (req, res) => {
   res.json(req.user);
 });
+
+router.get('/read', bearerMiddleware, permissions('read'), (req, res) => {
+  res.send('Route /read worked');
+});
+router.post('/add', bearerMiddleware, permissions('create'), (req, res) => {
+  res.send('Route /create worked');
+});
+router.put('/change', bearerMiddleware, permissions('update'), (req, res) => {
+  res.send('Route /update worked');
+});
+
+router.delete(
+  '/remove',
+  bearerMiddleware,
+  permissions('delete'),
+  (req, res) => {
+    res.send('Route /delete worked');
+  }
+);
 
 module.exports = router;
